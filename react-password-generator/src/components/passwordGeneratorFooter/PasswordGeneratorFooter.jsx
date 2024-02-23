0001f3a17ed06169533d31cfde7d009883bb8197
isuru <isuru.b@intervest.lk>
2024-02-23 12:50:21 +0530

    Complete footer

--- react-password-generator/src/components/passwordGeneratorFooter/PasswordGeneratorFooter.jsx
+++ react-password-generator/src/components/passwordGeneratorFooter/PasswordGeneratorFooter.jsx
@@ -1,4 +1,10 @@
 import css from "./style.module.css";
+import copyIcon from "../../assets/copy-paste.svg";
 export function PasswordGeneratorFooter() {
-  return <div className={css.root}>PasswordGeneratorFooter</div>;
+  return (
+    <div className={css.root}>
+      <div>Super Password</div>{" "}
+      <img className={css.icon} src={copyIcon} alt="Copy Paste" />
+    </div>
+  );
 }
